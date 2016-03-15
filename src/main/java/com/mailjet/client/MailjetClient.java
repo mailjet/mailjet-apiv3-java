5b553838f25230e21b7d544c06e565622ab223e9
Guillaume Badi <gbadi@mailjet.com>
2016-03-15 15:49:08 +0100

    version update

--- src/main/java/com/mailjet/client/MailjetClient.java
+++ src/main/java/com/mailjet/client/MailjetClient.java
@@ -88,7 +88,7 @@ public class MailjetClient {
         
         _client
               .addHeader("Accept", "application/json")
-              .addHeader("user-agent", "mailjet-apiv3-java/v3.1.0")
+              .addHeader("user-agent", "mailjet-apiv3-java/v3.1.1")
               .addHeader("Authorization", "Basic " + authEncBytes);
     }
     
